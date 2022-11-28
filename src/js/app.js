c5fadfbb3c93e1527cdb3fa195953dec25598bbc
Petar Dobrev <petar.dobrev92@gmail.com>
2022-11-28 22:47:16 +0200

    feat: fire emoji

--- src/js/app.js
+++ src/js/app.js
@@ -2,4 +2,12 @@ import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
+  const priceElements = document.querySelectorAll(".price");
+
+  priceElements.forEach((priceElement) => {
+    if (priceElement.classList.contains("hot")) {
+      const priceElementValue = priceElement.innerText;
+      priceElement.innerText = `${priceElementValue} 🔥`;
+    }
+  });
 });
